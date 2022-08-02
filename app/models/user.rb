309ada7133d2e524a82663798749abdf42f2db1d
Alex Wade <alexjohnwade@gmail.com>
2022-08-02 16:04:32 +1000

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
-  has_many :lessons
-  has_many :sessions, through: :lessons
+  has_many :products
+  has_many :notes, through: :products
   has_one_attached :avatar
 
   validates :avatar, file_size: { less_than_or_equal_to: 5.megabytes },
   file_content_type: { allow: ['image/jpeg', 'image/png', 'image/gif'] }
-
-
 end
